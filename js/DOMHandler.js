be91291f0c9e651d7c6d8758ba75373eb54fa802
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-13 21:30:51 -0500

    added a scroll bar to content div

--- js/DOMHandler.js
+++ js/DOMHandler.js
@@ -8,21 +8,17 @@ var initializeDOM = (function(){
 	window.addEventListener("keyup", function(event){
 		if (event.keyCode === 13){
 			Chatty.addMessageToDOM(Chatty.readInput(document.getElementById("messageTextInput").value));
-			console.log("key code registered");//call function to enter new message into the DOM
 		}
 	});
 //this listener listens for the large text for the body of the message.
 	largeTextCheckBox.addEventListener("change", function(event){
 		outputArea.classList.toggle("enlargeText"); //toggle the large text class for the text input section
-		console.log("text to big");
 	});
 	darkThemeCheckBox.addEventListener("change", function(event){	
 		document.body.classList.toggle("dark"); //if one is on to start  with only one will ever be on at a a time
-		console.log("theme to dark");
 	});
 	clearMessageButton.addEventListener("click", function(event){
 		//remove content from the content div in the html
 		outputArea.innerHTML = "";
-		console.log("cleared");
 	});
 })();
