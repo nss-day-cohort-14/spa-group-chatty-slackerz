22906397cb4ef9baf8159d73f6696dc3d105a276
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-14 14:07:33 -0500

    added edit functionality

--- js/editMessage.js
+++ js/editMessage.js
@@ -1,16 +1,6 @@
 var Chatty = (function(chatty){
 
-	chatty.editSelectedMessage = function(event) {
-		var targetMessage = event.currentTarget; //get the whole message (inc delete button)
-		var targetMessageID = targetMessage.id; //get the above message's id
-		var idNum = targetMessageID.split("--")[1]; 
-		var messageToEdit = document.getElementById(targetMessageID); //get the DOm element to delete
-
-		if (event.target.id === `editMessage--${idNum}`){
-			console.log("stuff");
-		}
-
-	};
+	
 
 	return chatty;
 })(Chatty || {});
