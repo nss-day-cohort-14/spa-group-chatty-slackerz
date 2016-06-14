8dfdded99adf26968e02c25a26f0572078434eee
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-14 11:45:17 -0500

    commented new file

--- js/messageLimiter.js
+++ js/messageLimiter.js
@@ -1,12 +1,12 @@
 var Chatty = (function (chatty){
 
-	chatty.limitMessages = function (idNum){
+	chatty.limitMessages = function (idNum){ //this method limit messages to 20
 		var messages = Chatty.getMessageArray(); 
 		var targetMessageID = `messageBlock--${idNum}`;
 
-		if(messages.length > 20){
-			messages.splice(0, 1);
-			document.getElementById("output").removeChild(document.getElementById("output").firstChild);
+		if(messages.length > 20){ //if the length is over 20 do:
+			messages.splice(0, 1); //slice the first element out
+			document.getElementById("output").removeChild(document.getElementById("output").firstChild); //remove the first child 
 		}	
 		return this.messages;
 	};
