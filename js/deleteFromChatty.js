ec9898be35efaa7d926488845641d476d29953bf
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-13 19:57:07 -0500

    Delete method for cards works

--- js/deleteFromChatty.js
+++ js/deleteFromChatty.js
@@ -1,5 +1,14 @@
 var Chatty = (function(chatty){
 
+	chatty.deleteMessage = function(event){
+		var targetMessage = event.currentTarget;
+		var targetMessageID = targetMessage.id;
+		var idNum = targetMessageID.split("--")[1];
+		var messageToDelete = document.getElementById(targetMessageID);
 
+		if (event.target.id === `messageButton--${idNum}`){
+			document.getElementById("output").removeChild(messageToDelete);
+		}
+	}
 	return chatty;
-})(Chatty || {});
+})(Chatty || {}); 
